8f8430c88f55bc6d62f61f174b5cecd1e1a639e8
PRAJESH <47695045+Prajesh-Srivastava@users.noreply.github.com>
2019-06-07 16:10:38 +0530

    Important One !!!
    Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.

---
+++ Caesar_Cipher.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+def caesar(n,s,k):
+    p=''
+    for i in s:
+        if ord(i)>=97 and ord(i)<=122:
+            w=ord(i)
+            
+            for j in range(k):
+                if w==122:
+                    w=97
+                else:
+                    w+=1
+            p=p+chr(w)
+        elif ord(i)>=65 and ord(i)<=90:
+            w=ord(i)
+            
+            for j in range(k):
+                if w==90:
+                    w=65
+                else:
+                    w+=1
+            p=p+chr(w)
+        else:
+            p+=i
+            continue
+    return(p)
+
+n=int(input())
+s=input()
+k=int(input())
+e=caesar(n,s,k)
+print(e)
